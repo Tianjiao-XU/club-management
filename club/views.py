911a6e4bc1b4ae97ec384ce994cebcdb32da1c4e
Fan Zhang <2750565z@student.gla.ac.uk>
2023-03-21 15:02:21 +0000

    update myclub

--- club/views.py
+++ club/views.py
@@ -1,12 +1,11 @@
 from datetime import datetime
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.http import JsonResponse
 from club.models import User, Club, Approval
 from club.forms import RegisterForm, LoginForm, CreateClubForm
 import json
