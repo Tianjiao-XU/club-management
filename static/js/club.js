5c306cebc5f1a8e6063f154648e4964ef99bf4fc
Tianjiao XU <2750450X@student.gla.ac.uk>
2023-03-21 12:29:40 +0000

    final information

--- static/js/club.js
+++ static/js/club.js
@@ -4,3 +4,15 @@
                 $("#div1").fadeOut("slow");
             });
         });
+
+        $(document).ready(function () {
+            $("img").click(function () {
+                $("#div2").fadeOut("slow");
+            });
+        });
+
+        $(document).ready(function () {
+            $("img").click(function () {
+                $("#div3").fadeOut("slow");
+            });
+        });
