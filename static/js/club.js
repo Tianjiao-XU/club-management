c5d2353f5bab81890db0346a3e4f392df05bc425
Tianjiao XU <2750450X@student.gla.ac.uk>
2023-03-22 15:40:06 +0000

    add comments

--- static/js/club.js
+++ static/js/club.js
@@ -1,27 +1,28 @@
+//click on img, fadeout slow
 $(document).ready(function () {
     $("img").click(function () {
         $("#div1").fadeOut("slow");
     });
 });
-
+//click on img, fadeout slow
 $(document).ready(function () {
     $("img").click(function () {
         $("#div2").fadeOut("slow");
     });
 });
-
+//click on img, fadeout slow
 $(document).ready(function () {
     $("img").click(function () {
         $("#div3").fadeOut("slow");
     });
 });
-
+//data now
 function showDate() {
   let now = new Date();
   let date = now.toLocaleDateString();
   document.getElementById('date').innerHTML = date;
 }
-
+//time now
 function showTime() {
   let now = new Date();
   let time = now.toLocaleTimeString();
